069980ba0a55ac4971f9eb51c71276e406a06f85
IlonaSkerberga <Iskerberga@outlook.com>
2021-08-21 10:20:50 +0100

    Create project

---
+++ SecondVkClient/ViewController/LogInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LogInViewController.swift
+//  SecondVkClient
+//
+//  Created by Ilona Skerberga on 20/08/2021.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    let session = Session.instance //singleton for storing data about the current session
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
